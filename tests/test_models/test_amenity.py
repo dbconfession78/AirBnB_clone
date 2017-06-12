8d6f0c7a75c344d2ea1fef0fecd470998812d0dd
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 22:15:16 +0000

    revertt

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,39 +1,131 @@
 #!/usr/bin/python3
+"""
+Module: Unit Testing for Amenity Class
+"""
 import unittest
 from models import amenity
-"""module: amenity_test"""
-
+from datetime import datetime
 
 Amenity = amenity.Amenity
 
 
 class TestAmenity(unittest.TestCase):
-    """Class: TestAmenity"""
+    """Test for presence of file, class and method documentation"""
+
     def setUp(self):
-        """instance setup"""
-        self.amenity = Amenity()
+        """ Base Model Documentation Testing Set-up"""
+        self.amenity = Amenity
+        print("\n")
+        print("=======================================")
+        print("               Amenity                 ")
+        print("           Documentation Tests         ")
+        print("=======================================")
+        print("")
 
-    def testattr(self):
-        """Testing the attributes of Amenity"""
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.assertFalse(hasattr(self.amenity, "updated_at"))
-        self.assertFalse(hasattr(self.amenity, "random_attr"))
-        self.assertTrue(hasattr(self.amenity, "name"))
-        self.assertEqual(self.amenity.__class__.__name__, "Amenity")
-        self.assertEqual(self.amenity.name, "")
+    def test_module_doc(self):
+        """ tests for presence module documentation """
+        expected = True
+        got = len(amenity.__doc__) > 0
+        self.assertEqual(expected, got)
 
-    def testmethod(self):
-        """Testing the methods of Amenity"""
-        self.amenity.save()
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
+    def test_class_doc(self):
+        """class documentation"""
+        expected = True
+        got = len(Amenity.__doc__) > 0
+        self.assertEqual(expected, got)
 
-    def teststr(self):
-        """Testing the str format of Amenity"""
-        s = "[{}] ({}) {}".format(self.amenity.__class__.__name__,
-                                  str(self.amenity.id),
-                                  self.amenity.__dict__)
-        self.assertEqual(print(s), print(self.amenity))
+    def test_save_doc(self):
+        """save() documentation"""
+        expected = True
+        got = len(Amenity.save.__doc__) > 0
+        self.assertEqual(expected, got)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_init_doc(self):
+        """__init__() documentation"""
+        expected = True
+        got = len(Amenity.__init__.__doc__) > 0
+        self. assertEqual(expected, got)
+
+    def test_to_json(self):
+        """to_json() documentation"""
+        expected = True
+        got = len(Amenity.to_json.__doc__) > 0
+        self.assertEqual(expected, got)
+
+    def test_str_doc(self):
+        """__str__() documentation"""
+        expected = True
+        got = len(Amenity.__str__.__doc__) > 0
+        self.assertEqual(expected, got)
+
+
+class TestInstantiation(unittest.TestCase):
+    """ test Amenity instantiation """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Amenity Instantiation Testing Set-up"""
+
+        print("\n")
+        print("=======================================")
+        print("                Amenity                ")
+        print("              Method Tests             ")
+        print("=======================================")
+        print("")
+
+    def test_instantiation(self):
+        """Amenity Instantiation"""
+        bm = Amenity()
+        self.assertIsInstance(bm, Amenity)
+
+    def test_string_format(self):
+        """String formatting"""
+        bm = Amenity()
+        expected = "[{}] ({}) {}".format(bm.__class__.__name__,
+                                         str(bm.id), bm.__dict__)
+        got = str(bm)
+        self.assertEqual(expected, got)
+
+    def test_save(self):
+        """call save()"""
+        bm = Amenity()
+        bm.save()
+        expected = True
+        got = bm.created_at != bm.updated_at
+        self.assertEqual(expected, got)
+
+    def test_to_json(self):
+        """call to_json"""
+        bm = Amenity()
+        json = str(bm.to_json())
+        created_at = "'created_at': '" + datetime.strftime(
+            bm.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f") + "'"
+        header = "{'__class__': 'Amenity', 'id': '"
+        _id = bm.__dict__["id"]
+        string = "{'__class__': 'Amenity', 'id': '" + bm.__dict__[
+            "id"] + "', 'created_at': '" + created_at + "'}"
+        expected = True
+        if created_at in string and header in string and _id in string:
+            got = True
+        else:
+            got = False
+
+        self.assertEqual(expected, got)
+
+    def test_name_attr(self):
+        """name attribute"""
+        bm = Amenity()
+        expected = "XXX"
+        got = "XXX"
+
+        self.assertEqual(expected, got)
+
+    def test_number_attr(self):
+        """number attribute"""
+        bm = Amenity()
+        expected = "XXX"
+        got = "XXX"
+        self.assertEqual(expected, got)
+
+if __name__ == "__main__":
+    unittest.main
