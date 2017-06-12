e209479d7259284a7e26f9cbd134987dbb58b3ec
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 19:33:05 +0000

    test

--- models/state.py
+++ models/state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Module: state
+State Class from Models Module
 """
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    State class
-    """
-    name = ""
+    """State class handles all application states"""
+
+    name = ''
 
     def __init__(self, *args, **kwargs):
-        """initializes instance of State"""
+        """instantiates a new state"""
         super().__init__(*args, **kwargs)
