3bb3f4e76e8ae7f261da3c652f3b9dbc4d949a2f
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 18:44:22 +0000

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,59 +1,54 @@
 #!/usr/bin/python3
 """
-Module: base_model
+Module for base model
 """
-import uuid
+
+
+from uuid import uuid4
 from datetime import datetime
 import models
 
-dt_format = "%Y-%m-%dT%H:%M:%S.%f"
-
 
 class BaseModel:
-    """
-    BaseModel Class
-    """
+    """ defines BaseModel class """
+
+    timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+
+    """ initializes instance """
     def __init__(self, *args, **kwargs):
-        """
-        BaseModel initializer
-        - if k/w args passed in, set class dict to args
-        - if k/w args not passed in, set new uuid and current time
-        """
-        if kwargs:
-            if "created_at" in kwargs:
-                self.__dict__ = kwargs
+        if len(kwargs) > 0:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     self.timeformat)
+            if "updated_at" in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         self.timeformat)
+            self.__dict__ = kwargs
         else:
-            self.id = str(uuid.uuid4())  # obj to str
-            self.created_at = datetime.now()  # obj
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
             models.storage.new(self)
 
+    """ updates attribute updated_at with current datetime """
     def save(self):
-        """ updates the public instance attribute
-        `updated_at` with the current datetime
-        """
-        self.updated_at = datetime.now()  # as obj
+        self.updated_at = datetime.now()
         models.storage.save()
 
+    """ returns dictionary of all keys/values of instance + the class name """
     def to_json(self):
-        """
-        - returns a dictionary containing all class instance keys/values
-          + class name in key `__class__`.
-        - This method will be the first piece of
-          the serialization/deserialization process.
-        """
-        json = {}
-        json["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, datetime):
-                json[k] = v.isoformat()
-            else:
-                json[k] = v
-        return json
+        new_dict = self.__dict__.copy()
+        new_dict.update({'__class__': str(self.__class__.__name__)})
+        for key, value in new_dict.items():
+            if isinstance(value, datetime):
+                new_dict[key] = value.strftime(self.timeformat)
+        return new_dict
 
+    """ prints dictionary of attributes of the instance """
     def __str__(self):
-        """
-        returns class, id and class attributes format for
-        printing as string
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, str(self.id),
-                                     self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    """ prints dictionary of attributes of the instance """
+    def __repr__(self):
+        return self.__str__()
