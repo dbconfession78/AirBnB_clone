9f8467cafa6ebb9be5fcb00ca122e9dc2973cd95
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 22:23:56 +0000

    add argument condition in init

--- models/user.py
+++ models/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Module: user"""
 from models.base_model import BaseModel
+"""module: class User"""
 
 
 class User(BaseModel):
-    """User class"""
+    '''class User'''
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes intance of User"""
-        super().__init__(self, *args, **kwargs)
+        '''method: init'''
+        if (kwargs.get('id') is not None):
+            self.__dict__ = kwargs
+        else:
+            super().__init__(self)
