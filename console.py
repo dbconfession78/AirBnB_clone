163eb483b263b98042c80236edeb595da6ef4cfa
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 23:25:39 +0000

    made it so if no class  was provided, error message displayed

--- console.py
+++ console.py
@@ -102,13 +102,13 @@ class HBNBCommand(cmd.Cmd):
         if name:
             if name in class_models:
                 for k, v in store.items():
-                    if name in k:
+                    if type(v).__name__ == class_models[arg[0]].__name__:
+#                    if name in k:
                         print(v)
             else:
                 print("** class doesn't exist **")
         else:
-            for k, v in store.items():
-                print(v)
+            print("** class doesn't exist **")
 
     def do_update(self, args):
         """- Updates instance based on class name+id by adding/updating attr
