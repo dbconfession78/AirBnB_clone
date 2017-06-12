ddbdc39075b233b7f0a044df25a41745ddc895ca
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 20:39:40 +0000

    revert

--- models/user.py
+++ models/user.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+"""Module: user"""
 from models.base_model import BaseModel
-"""module: class User"""
 
 
 class User(BaseModel):
-    '''class User'''
+    """User class"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __init__(self, *args, **kwargs):
-        '''method: init'''
-        if (kwargs.get('id') is not None):
-            self.__dict__ = kwargs
-        else:
-            super().__init__(self)
+        """initializes intance of User"""
+        super().__init__(self, *args, **kwargs)
