40e6bf5673d6287a5cdabdf4ba6d42d5776e9a78
Stuart Kuredjian <108@holbertonschool.com>
2017-06-12 18:43:55 +0000

    remove imports other than FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -4,15 +4,5 @@ Module: __init__
 initializer for the `models` module
 """
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.review import Review
-from models.user import User
-from models.place import Place
-
-
-
 storage = FileStorage()
 storage.reload()
