c20ad96e442fb6bd6024f654d659709b05ee6b51
Stuart Kuredjian <108@holbertonschool.com>
2017-06-13 03:22:28 +0000

    fix type in number_bathrooms

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ class Place(BaseModel):
     name = ""
     description = ""
     number_rooms = 0
-    number_bathrroms = 0
+    number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
