fd9ccf2cbf498de34144ec3785387366bb1a98ee
Stuart Kuredjian <108@holbertonschool.com>
2017-06-13 02:03:54 +0000

    test_console.py: initial commit

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
 """Module: test_console"""
+from unittest.mock import create_autospec
 import unittest
-import models
 from console import HBNBCommand
-from datetime import datetime
-from models.base_model import BaseModel
-
+import sys
 
 class TestConsole(unittest.TestCase):
     """
     Console testing
     """
-    console = HBNBCommand()
+    def setUp(self):
+        """setup mock stdin and stdout"""
+        self.stdout = create_autospec(sys.stdout)
+        self.stdin = create_autospec(sys.stdin)
 
-    def create(self):
-        """ test create"""
-        self.console = HBNBCommand()
-        args = {'updated_at': datetime(2017, 6, 11, 10, 47, 00, 123456),
-                'id': '6493e0f4-0251-4a20-93c0-339ea262fe0e',
-                'created_at': datetime(2017, 6, 11, 17, 50, 16, 307880),
-                'name': 'SELF_MODEL'}
-        self.model = BaseModel(**args)
-        self.model.save()
-
-    def destroy(self):
-        """ test destroy"""
-        self.console.do_destroy(
-            "BaseModel 6493e0f4-0251-4a20-93c0-339ea262fe0e")
+    def create(self, server=None):
+        """instantiate the console"""
+        return HBNBCommand(stdin=self.stdin, stdout=self.stdout)
 
     def test_quit(self):
         """test quit"""
-        with self.assertRaises(SystemExit):
-            self.console.do_quit(self.console)
+        console = self.create()
+        expected = True
+        got = console.do_quit("")
+        self.assertEqual(expected, got)
