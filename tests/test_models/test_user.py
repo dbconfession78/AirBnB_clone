597b9537f167fe26ac2f507cce712ddbbd6f6b88
Stuart Kuredjian <108@holbertonschool.com>
2017-06-13 03:33:34 +0000

    add test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,32 +1,52 @@
 #!/usr/bin/python3
 """
-Module: Unit Testing for User Class
+Module: Unit Testing for User class
 """
 import unittest
-from moels import user
-from datetime import datetime
-
-User = user.User
+from models.user import User
 
 
-class TestDocumentation(unittest.TestCase):
-    """Test for presence of file, class and method documentation"""
+class TestUser(unittest.TestCase):
+    """TestUser class"""
+    def setUp(self):
+        """ instantiate class"""
+        self.cls = User()
 
-    @classmethod
-    def setUpClass(cls):
-        """ Documentation Testing Set-up"""
+    def testattr(self):
+        """ testing  attributes"""
+        self.assertTrue(hasattr(self.cls, "created_at"))
+        self.assertTrue(hasattr(self.cls, "id"))
+        self.assertFalse(hasattr(self.cls, "updated_at"))
+        self.assertFalse(hasattr(self.cls, "random_attr"))
 
-        print("\n")
-        print("=======================================")
-        print("                 User                  ")
-        print("           Documentation Tests         ")
-        print("=======================================")
-        print("")
+        self.assertTrue(hasattr(self.cls, "email"))
+        self.assertTrue(hasattr(self.cls, "password"))
+        self.assertTrue(hasattr(self.cls, "first_name"))
+        self.assertTrue(hasattr(self.cls, "last_name"))
+
+        self.assertEqual(self.cls.__class__.__name__, "User")
+        self.assertEqual(self.cls.first_name, "")
+
+        self.cls.first_name = "Betty"
+        self.assertEqual(self.cls.first_name, "Betty")
+
+    def test_method(self):
+        """testing Amenity methods"""
+        self.cls.save()
+        self.assertTrue(hasattr(self.cls, "updated_at"))
+
+    def test_string_format(self):
+        """testing string formatting"""
+        expected = "[{}] ({}) {}".format(
+            self.cls.__class__.__name__, str(
+                self.cls.id), self.cls.__dict__)
+        got = str(self.cls)
+        self.assertEqual(expected, got)
 
     def test_module_doc(self):
         """ module documentation """
         expected = True
-        got = len(user.__doc__) > 0
+        got = len(self.cls.__doc__) > 0
         self.assertEqual(expected, got)
 
     def test_class_doc(self):
@@ -41,48 +61,14 @@ class TestDocumentation(unittest.TestCase):
         got = len(User.__init__.__doc__) > 0
         self. assertEqual(expected, got)
 
-
-class MethodTests(unittest.TestCase):
-    """ test class methods and instantiation """
-
-    @classmethod
-    def setUpClass(cls):
-        """ Method Testing Set-up"""
-
-        print("\n")
-        print("=======================================")
-        print("                 User                  ")
-        print("              Method Tests             ")
-        print("=======================================")
-        print("")
-
-    def test_instantiation(self):
-        """ Instantiation"""
-        cls = User()
-        self.assertIsInstance(cls, User)
-
-    def test_string_format(self):
-        """String formatting"""
-        cls = User()
-        expected = "[{}] ({}) {}".format(cls.__class__.__name__,
-                                         str(cls.id), cls.__dict__)
-        got = str(cls)
+    def test_str_doc(self):
+        """__str__() documentation"""
+        expected = True
+        got = len(User.__str__.__doc__) > 0
         self.assertEqual(expected, got)
 
-    def test_name_attr(self):
-        """name attribute"""
-        cls = User()
-        expected = "XXX"
-        got = "XXX"
-
+    def test_to_json(self):
+        """to_json() documentation"""
+        expected = True
+        got = len(User.to_json.__doc__) > 0
         self.assertEqual(expected, got)
-
-    def test_number_attr(self):
-        """number attribute"""
-        cls = User()
-        expected = "XXX"
-        got = "XXX"
-        self.assertEqual(expected, got)
-
-if __name__ == "__main__":
-    unittest.main
