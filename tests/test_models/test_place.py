4eb766566815f393fc208bd98925d49e86a99216
Stuart Kuredjian <108@holbertonschool.com>
2017-06-13 03:21:59 +0000

    add test cases

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,32 +1,59 @@
 #!/usr/bin/python3
 """
-Module: Unit Testing for Place Class
+Module: Unit Testing for Place class
 """
 import unittest
-from models import place
-from datetime import datetime
-
-Place = place.Place
+from models.place import Place
 
 
-class TestDocumentation(unittest.TestCase):
-    """Test for presence of file, class and method documentation"""
+class TestPlace(unittest.TestCase):
+    """TestPlace class"""
+    def setUp(self):
+        """ instantiate class"""
+        self.cls = Place()
 
-    @classmethod
-    def setUpClass(cls):
-        """ Documentation Testing Set-up"""
+    def testattr(self):
+        """ testing  attributes"""
+        self.assertTrue(hasattr(self.cls, "created_at"))
+        self.assertTrue(hasattr(self.cls, "id"))
+        self.assertFalse(hasattr(self.cls, "updated_at"))
+        self.assertFalse(hasattr(self.cls, "random_attr"))
 
-        print("\n")
-        print("=======================================")
-        print("                 Place                 ")
-        print("           Documentation Tests         ")
-        print("=======================================")
-        print("")
+        self.assertTrue(hasattr(self.cls, "city_id"))
+        self.assertTrue(hasattr(self.cls, "user_id"))
+        self.assertTrue(hasattr(self.cls, "name"))
+        self.assertTrue(hasattr(self.cls, "description"))
+        self.assertTrue(hasattr(self.cls, "number_rooms"))
+        self.assertTrue(hasattr(self.cls, "number_bathrooms"))
+        self.assertTrue(hasattr(self.cls, "max_guest"))
+        self.assertTrue(hasattr(self.cls, "price_by_night"))
+        self.assertTrue(hasattr(self.cls, "latitude"))
+        self.assertTrue(hasattr(self.cls, "longitude"))
+        self.assertTrue(hasattr(self.cls, "amenity_ids"))
+
+        self.assertEqual(self.cls.__class__.__name__, "Place")
+        self.assertEqual(self.cls.name, "")
+
+        self.cls.name = "Red House"
+        self.assertEqual(self.cls.name, "Red House")
+
+    def test_method(self):
+        """testing Amenity methods"""
+        self.cls.save()
+        self.assertTrue(hasattr(self.cls, "updated_at"))
+
+    def test_string_format(self):
+        """testing string formatting"""
+        expected = "[{}] ({}) {}".format(
+            self.cls.__class__.__name__, str(
+                self.cls.id), self.cls.__dict__)
+        got = str(self.cls)
+        self.assertEqual(expected, got)
 
     def test_module_doc(self):
         """ module documentation """
         expected = True
-        got = len(place.__doc__) > 0
+        got = len(self.cls.__doc__) > 0
         self.assertEqual(expected, got)
 
     def test_class_doc(self):
@@ -41,48 +68,14 @@ class TestDocumentation(unittest.TestCase):
         got = len(Place.__init__.__doc__) > 0
         self. assertEqual(expected, got)
 
-
-class MethodTests(unittest.TestCase):
-    """ test class methods and instantiation """
-
-    @classmethod
-    def setUpClass(cls):
-        """ Method Testing Set-up"""
-
-        print("\n")
-        print("=======================================")
-        print("                 Place                 ")
-        print("              Method Tests             ")
-        print("=======================================")
-        print("")
-
-    def test_instantiation(self):
-        """ Instantiation"""
-        cls = Place()
-        self.assertIsInstance(cls, Place)
-
-    def test_string_format(self):
-        """String formatting"""
-        cls = Place()
-        expected = "[{}] ({}) {}".format(cls.__class__.__name__,
-                                         str(cls.id), cls.__dict__)
-        got = str(cls)
+    def test_str_doc(self):
+        """__str__() documentation"""
+        expected = True
+        got = len(Place.__str__.__doc__) > 0
         self.assertEqual(expected, got)
 
-    def test_name_attr(self):
-        """name attribute"""
-        cls = Place()
-        expected = "XXX"
-        got = "XXX"
-
+    def test_to_json(self):
+        """to_json() documentation"""
+        expected = True
+        got = len(Place.to_json.__doc__) > 0
         self.assertEqual(expected, got)
-
-    def test_number_attr(self):
-        """number attribute"""
-        cls = Place()
-        expected = "XXX"
-        got = "XXX"
-        self.assertEqual(expected, got)
-
-if __name__ == "__main__":
-    unittest.main
