639d9e3c5dd8a4669947210d56106295f4bc6aa5
Stuart Kuredjian <108@holbertonschool.com>
2017-06-14 03:51:51 +0000

    add test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import unittest
 from models.engine.file_storage import FileStorage
+from models import storage
 """Module: test_file_storage """
 
 
@@ -13,6 +14,12 @@ class TestFileStorage(unittest.TestCase):
     def test_attrs(self):
         """testing the attributes of FileStorage"""
         self.assertFalse(hasattr(self.cls, "random_attr"))
+        self.assertTrue(hasattr(self.cls, "class_models"))
+
+
+    def test_instqntiation(self):
+        """ instatiate class """
+        self.assertIsInstance(storage, FileStorage)
 
     def test_module_doc(self):
         """ module documentation """
@@ -26,11 +33,35 @@ class TestFileStorage(unittest.TestCase):
         got = len(FileStorage.__doc__) > 0
         self.assertEqual(expected, got)
 
-        def test_init_doc(self):
-            """__init__() documentation"""
-            expected = True
-            got = len(FileStorage.__init__.__doc__) > 0
-            self.assertEqual(expected, got)
+    def test_init_doc(self):
+        """__init__() documentation"""
+        expected = True
+        got = len(FileStorage.__init__.__doc__) > 0
+        self.assertEqual(expected, got)
+
+    def test_all_doc(self):
+        """ all() method documentation"""
+        expected = True
+        got = len(FileStorage.all.__doc__) > 0
+        self.assertEqual(expected, got)
+
+    def test_new_doc(self):
+        """ new() method documentation """
+        expected = True
+        got = len(FileStorage.new.__doc__) > 0
+        self.assertEqual(expected, got)
+
+    def test_new_doc(self):
+        """ save() method documentation """
+        expected = True
+        got = len(FileStorage.save.__doc__) > 0
+        self.assertEqual(expected, got)
+
+    def test_reload_doc(self):
+        """ reload() method documentation """
+        expected = True
+        got = len(FileStorage.reload.__doc__) > 0
+        self.assertEqual(expected, got)
 
 if "__main__" == __name__:
     unittest.main()
