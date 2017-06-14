42da14e3442ad4db98b252604d22f275fc54e66a
Stuart Kuredjian <108@holbertonschool.com>
2017-06-14 01:34:35 +0000

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,70 @@
 #!/usr/bin/python3
 """
-Module: base_model
+BaseModel Class of Models Module
 """
 
-import uuid
-from datetime import datetime
+import json
 import models
+from uuid import uuid4, UUID
+from datetime import datetime
+
+now = datetime.now
+strptime = datetime.strptime
 
 
 class BaseModel:
-    """BaseModel Class"""
-
-    dt_format = "%Y-%m-%dT%H:%M:%S.%f"
+    """attributes and functions for BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        """initializes new instance of BaseModel"""
+        """instantiation of new BaseModel Class"""
         if kwargs:
-            kwargs = self.sanitize_deserialization(kwargs)
-            self.__dict__ = kwargs
+            self.__set_attributes(kwargs)
         else:
-            self.id = str(uuid.uuid4())  # obj -> str
-            self.created_at = datetime.now()  # as obj
+            self.id = str(uuid4())
+            self.created_at = now()
             models.storage.new(self)
 
+    def __set_attributes(self, d):
+        """converts kwargs values to python class attributes"""
+        if not isinstance(d['created_at'], datetime):
+            d['created_at'] = strptime(d['created_at'], "%Y-%m-%d %H:%M:%S.%f")
+        if 'updated_at' in d:
+            if not isinstance(d['updated_at'], datetime):
+                d['updated_at'] = strptime(d['updated_at'],
+                                           "%Y-%m-%d %H:%M:%S.%f")
+        if d['__class__']:
+            d.pop('__class__')
+        self.__dict__ = d
+
+    def __is_serializable(self, obj_v):
+        """checks if object is serializable"""
+        try:
+            nada = json.dumps(obj_v)
+            return True
+        except:
+            return False
+
+    def bm_update(self, name, value):
+        setattr(self, name, value)
+        self.save()
+
     def save(self):
-        """ updates the public instance attribute
-        `updated_at` with the current datetime
-        """
-        self.updated_at = datetime.now()  # as obj
+        """updates attribute updated_at to current time"""
+        self.updated_at = now()
         models.storage.save()
 
     def to_json(self):
-        """returns objects converted to JSON format"""
-        json = {}
-        json["__class__"] = type(self).__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, datetime):
-                json[k] = v.isoformat()
+        """returns json representation of self"""
+        bm_dict = {}
+        for k, v in (self.__dict__).items():
+            if (self.__is_serializable(v)):
+                bm_dict[k] = v
             else:
-                json[k] = v
-        return json
-
-    def sanitize_deserialization(self, kwargs):
-        """deserializes datetime attributes if necessary"""
-        if not isinstance(kwargs["created_at"], datetime):
-                kwargs["created_at"] = datetime.strptime(
-                    kwargs["created_at"], self.dt_format)
-
-        if "updated_at" in kwargs:
-            if not isinstance(kwargs["updated_at"], datetime):
-                kwargs["updated_at"] = datetime.strptime(
-                    kwargs["updated_at"], self.dt_format)
-        return kwargs
+                bm_dict[k] = str(v)
+        bm_dict["__class__"] = type(self).__name__
+        return(bm_dict)
 
     def __str__(self):
-        """
-        returns class, id and class attributes format for
-        printing as string
-        """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id,
-                                     self.__dict__)
+        """returns string type representation of object instance"""
+        cname = type(self).__name__
+        return "[{}] ({}) {}".format(cname, self.id, self.__dict__)
